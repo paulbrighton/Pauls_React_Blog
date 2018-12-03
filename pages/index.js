1317fe1e190a2d27f360daddbfbadb63e49e5cf4
Paul Brighton <brighton.paul@yahoo.com>
2018-12-03 10:28:36 +0000

    Feature/react blog setup (#1)
    * File system and tests setup
    
    * Files imported from rails and footer added
    
    * Moved fonts and images to static folder

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+
+import Layout from '../components/Layout'
+
+class Index extends Component {
+  render () {
+    return <Layout {...this.props}>   
+    </Layout>
+  }
+}
+
+export default Index
