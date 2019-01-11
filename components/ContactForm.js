6408469fb8b0553a8adad7a5c9a2179b0cb528ff
Paul Brighton <brighton.paul@yahoo.com>
2019-01-11 13:17:53 +0000

    Added postmark to contact form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,15 +1,37 @@
 import { Component } from 'react'
+import fetch from 'isomorphic-unfetch'
 
 class ContactForm extends Component {
+  async handleSubmit (event) {
+    event.preventDefault()
+    const form = event.target
+    const formData = {
+      From: 'paul@paulbrighton.com',
+      To: 'paul@paulbrighton.com',
+      ReplyTo: form.email.value,
+      HtmlBody: form.body.value,
+      Name: form.name.value
+    }
+    const response = await fetch('/contact',
+      {
+        credentials: 'same-origin',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      })
+    if (response.status === 200) {
+      form.reset()
+    }
+  }
   render () {
     return <section className='contact-form'>
       <h3>Contact Form</h3>
       <p>Fill in the contact form and I'll get back to you as soon as possible.</p>
       <fieldset>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type='text' placeholder='Enter name here' name='name' className='contact-form__input--short' required />
           <input type='email' placeholder='Enter email here' name='email' className='contact-form__input--short' required />
-          <textarea placeholder='Enter message here.' className='contact-form__input--tall' name='body' />
+          <textarea placeholder='Enter message here.' className='contact-form__input--tall' name='body' required />
           <input type='submit' value='Send' className='contact-form__btn' />
         </form>
       </fieldset>
--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const postmark = require('postmark')
+const bodyParser = require('body-parser')
 
 app.prepare()
   .then(() => {
     const server = express()
+    server.use(bodyParser.json())
 
     server.get('/', (req, res) => {
       return app.render(req, res, '/', req.query)
@@ -37,5 +40,27 @@ app.prepare()
     server.listen(port, (err) => {
       if (err) throw err
       console.log(`> Ready on http://localhost:${port}`)
+    })
+
+    server.post('/contact', (req, res) => {
+      const client = new postmark.ServerClient(`${process.env.POSTMARK_API_KEY}`)
+
+      client.sendEmail(
+        {
+          From: 'paul@paulbrighton.com',
+          To: 'paul@paulbrighton.com',
+          Subject: req.body.Name,
+          HtmlBody: req.body.HtmlBody,
+          ReplyTo: req.body.ReplyTo
+        }
+      ).then(response => {
+        console.log('Sending message')
+        console.log(response.To)
+        console.log(response.Message)
+        return res.sendStatus(200)
+      }).catch(error => {
+        console.log(error)
+        return res.sendStatus(500)
+      })
     })
   })
