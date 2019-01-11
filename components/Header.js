90287c8eb80ef7b8cd77047b2c52ccef6c8555d8
Paul Brighton <brighton.paul@yahoo.com>
2019-01-11 14:31:19 +0000

    Added postmark to contact form (#20)

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,8 @@ class Header extends Component {
       <div className='o-header__container'>
         <Link href='/index'><a><img src='/static/images/logo.png' alt='Paul Brighton Web Development Logo' className='c-brand-logo' /></a></Link>
         <div className='c-subscribe-form'>
-          <form className='c-subscribe-form__container'>
-            <input type='email' value='Email' placeholder='Email' name='EMAIL' className='c-subscribe-form__input' id='mce-EMAIL' />
+          <form action='https://twitter.us17.list-manage.com/subscribe/post?u=5e14f42c2626123e850bb543f&amp;id=5dd22b22a9' method='post' name='mc-embedded-subscribe-form' className='c-subscribe-form__container' target='_blank' noValidate>
+            <input type='email' placeholder='Email' name='EMAIL' className='c-subscribe-form__input' id='mce-EMAIL' />
             <input type='submit' value='Subscribe' name='subscribe' className='c-subscribe-form__btn' />
           </form>
         </div>
