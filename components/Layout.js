e610e4f77c8acc80d0350b6e92b0bfb677806a65
Paul Brighton <brighton.paul@yahoo.com>
2019-01-15 15:51:59 +0000

    Improved styling for articles and home page (#35)

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,7 @@ class Layout extends Component {
         <title>Paul Brighton's Web Development Blog</title>
         <meta name='description' content='A blog by Paul Brighton all about web development. It includes articles about HTML, CSS, JavaScript and Ruby on Rails plus other tips on web design and developing.' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='shortcut icon' href='/static/images/favicon.ico' type='image/x-icon' />
       </Head>
       <Header />
       { this.props.children }
