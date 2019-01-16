1b139645360e9c4010489065f1c7242a31708ebb
Paul Brighton <brighton.paul@yahoo.com>
2019-01-16 10:11:36 +0000

    Changed date format for moment

--- components/Article.js
+++ components/Article.js
@@ -13,7 +13,7 @@ class Article extends Component {
         <h2 className='c-article__header'>{article.title}</h2>
         <div className='c-header-divider' />
         <h3 className='c-article__author'>Article by: {article.author}</h3>
-        <time className='c-article__published'><p>Published on: {moment(article.createdAt).format('ll')}</p></time>
+        <time className='c-article__published'><p>Published on: {moment(article.createdAt).format('Do MMMM YYYY')}</p></time>
         <figure>
           <img className='c-article__main-img' src={`https://pauls-blog.herokuapp.com/${article.image}`} />
         </figure>
