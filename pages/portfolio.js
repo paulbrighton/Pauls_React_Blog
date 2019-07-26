73e7aab298b6feeb58eda2feb1c9b290cc7abcf4
Paul Brighton <brighton.paul@yahoo.com>
2019-07-26 17:12:29 +0100

    Updated portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -40,7 +40,7 @@ class Portfolio extends Component {
           <div className='c-portfolio-project__text'>
             <p>I have been wanting to get into ecommerce web development for a while so decided to do the Wes Bos Full Stack Advanced React and Graphql course.</p>
             <p>The backend was built with Graphql Yoga and Prisma and the front end is ReactJS and NextJS with Apollo Client and tests were added using Jest. The site features include pagination, taking payments with Stripe and tour search functionality.</p>
-            <p>The site can be viewed at <a href='https://sick-fits-front-end.herokuapp.com/' className='c-portfolio-project__link' target='blank'>Day Tripper Tours</a> but sorry, you won't be able to book any of the tours for real as it's just a portfolio piece.</p>
+            <p>The site can be viewed at <a href='https://sick-fits-front-end.herokuapp.com/' className='c-portfolio-project__link' target='blank'>Day Tripper Tours</a> but you will need to sign up for an account (using any fake email should be fine) and login for full functionality.</p>
           </div>
         </div>
 
